a6af8c1af9f0021a9c698b484d30c3f230ec9682
Seelam <pavankalyan.seelam@effem.com>
2025-07-20 12:03:15 +0530

    Update backend.tf configuration

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket         = "pk-tf-state-bucket"
-    key            = "dev/terraform.tfstate"
+    key = "dev-pk/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
   }
